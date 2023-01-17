4dc3bd6da17b441c22cfde2ac3e8451b3a396ade
Udalov Anton <nt.dlv@list.ru>
2023-01-17 22:40:01 +0300

    final version with O(N+K*logN)

--- Polygon.h
+++ Polygon.h
@@ -10,13 +10,16 @@
 
 struct Vec {
     double x, y;
+
     Vec();
+
     Vec(double x, double y);
+
     ~Vec();
 };
 
 
-Vec::~Vec(){}
+Vec::~Vec() {}
 
 
 std::ostream &operator<<(std::ostream &s, Vec &vec) {
@@ -25,41 +28,37 @@ std::ostream &operator<<(std::ostream &s, Vec &vec) {
 }
 
 
-bool operator>(const Vec &v1, const Vec &v2) {
-    if (v1.x > v2.x) {
-        return true;
-    } else return false;
-}
-
-
 class Polygon {
 public:
     int n_vertex;
     std::vector<Vec> vertex;
+
     Polygon();
+
     Polygon(int n, std::vector<Vec> vertexes);
+
     ~Polygon();
+
     Vec left_vertex(Polygon &p);
+
     std::vector<double> polar_coord(std::vector<Vec> &p, Vec &lv);
+
     bool Algorithm(Polygon &p, std::vector<Vec> &dots);
+
     double polar_coord_for_dot(Vec &dot, Vec &lv);
 
     int ceilSearch(Polygon &arr, int low, int high, Vec &x, Vec &l);
 };
 
-Polygon::~Polygon() { }
+Polygon::~Polygon() {}
 
 Vec::Vec() : x(), y() {}
 
-
 Vec::Vec(double x, double y) : x(x), y(y) {}
-
 
 Polygon::Polygon() : n_vertex(), vertex() {}
 
-
 Polygon::Polygon(int n, std::vector<Vec> vertexes) : n_vertex(n), vertex(std::move(vertexes)) {}
-
 
 Vec Polygon::left_vertex(Polygon &p) {
     int n = p.n_vertex;
@@ -80,7 +79,6 @@ Vec Polygon::left_vertex(Polygon &p) {
     return lv;
 }
 
-
 std::vector<double> Polygon::polar_coord(std::vector<Vec> &p, Vec &lv) {
     std::vector<double> coord(p.size()); //vector of polar coordinates of vertexes
     std::vector<Vec> a_b(p);
@@ -92,7 +90,6 @@ std::vector<double> Polygon::polar_coord(std::vector<Vec> &p, Vec &lv) {
     return coord;
 }
 
-
 double Polygon::polar_coord_for_dot(Vec &dot, Vec &lv) {
     double pd;
     Vec td(dot.x - lv.x, dot.y - lv.y);
@@ -100,46 +97,26 @@ double Polygon::polar_coord_for_dot(Vec &dot, Vec &lv) {
     return pd;
 }
 
-
-
 double check(Vec &a, Vec &b, Vec &c) {
     return a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
 }
-
 
 bool Polygon::Algorithm(Polygon &p, std::vector<Vec> &dots) {
     Vec lv = p.left_vertex(p); // N
     for (int d = 0; d < dots.size(); ++d) {
         int pos = ceilSearch(p, 0, p.vertex.size(), dots[d], lv);
         if (pos == -1) {
-            //std::cout << p.vertex[pos] <<" " <<p.vertex[pos+1] <<" " << dots[d] << std::endl;
-            //std::cout << pc[pos] << " ! " << pc[pos+1] << " " << pc[d] << std::endl;
-            std::cout << "(" << dots[d] <<") " << "out" << std::endl;
+            std::cout << "(" << dots[d] << ") " << "out" << std::endl;
         } else {
             int pos_ = pos;
             pos -= 1;
-            //std::cout << n_vertex << std::endl;
-            //std::cout << p.vertex[n_vertex] << std::endl;
-            //std::cout << "pos is: " << pos << std::endl;
-            //std::cout << "pos_ is: " << pos_ << std::endl;
-            //std::cout << p.vertex[pos] << " ; " << p.vertex[pos_] << std::endl;
-            //std::cout << pc[pos] << " ; " << pc[pos_] << "; " << pd[d] << std::endl;
-            //std::cout << pos << " " << pos_ << std::endl;
             if (pos_ < p.n_vertex) {
-                //std::cout << p.vertex[pos] <<" " <<p.vertex[pos_] <<" " << dots[d] << std::endl;
                 if (check(p.vertex[pos], p.vertex[pos_], dots[d]) >= 0) {
-                    //std::cout << p.vertex[pos-1] <<" " <<p.vertex[pos_-1] <<" " << dots[d] << std::endl;
-                    //std::cout << p.vertex[pos] <<" " <<p.vertex[pos_] <<" " << dots[d] << std::endl;
-                    //std::cout << pos << " ! " <<pos_ << " " << pc[d] << std::endl;
-                    //std::cout << pc[pos] << " ! " << pc[pos_] << " " << pc[d] << std::endl;
                     std::cout << "(" << dots[d] << ") " << "in" << std::endl;
                 } else {
-                    //std::cout << p.vertex[pos] <<" " <<p.vertex[pos_] <<" " << dots[d] << std::endl;
-                    //std::cout << pc[pos] << " ! " << pc[pos_] << " " << pc[d] << std::endl;
                     std::cout << "(" << dots[d] << ") " << "out" << std::endl;
                 }
-        } else {
-                //std::cout << "priv" << std::endl;
+            } else {
                 if (check(p.vertex[pos], lv, dots[d]) >= 0) {
                     std::cout << "(" << dots[d] << ") " << "in" << std::endl;
                 } else {
@@ -150,16 +127,10 @@ bool Polygon::Algorithm(Polygon &p, std::vector<Vec> &dots) {
     }
 }
 
-
-int Polygon::ceilSearch(Polygon& arr, int low, int high, Vec& x, Vec& l) {
-
-    //std::cout << "x " << x << std::endl;
-    //std::cout << "arr[0] " << arr[low] << std::endl;
+int Polygon::ceilSearch(Polygon &arr, int low, int high, Vec &x, Vec &l) {
     double px = polar_coord_for_dot(x, l);
-    //std::cout << px << std::endl;
 
     if (px < polar_coord_for_dot(arr.vertex[low], l)) {
-        //std::cout << "=)" <<std::endl;
         return -1;
     }
     int mid;
@@ -173,11 +144,7 @@ int Polygon::ceilSearch(Polygon& arr, int low, int high, Vec& x, Vec& l) {
             low = mid + 1;
         }
     }
-    //std::cout << "low is: " << low << std::endl;
-    //std::cout << "mid is: " << mid << std::endl;
-    //std::cout << "high is: " << high << std::endl;
     return low;
 }
-
 
 #endif //CHECKPOINTS_POLYGON_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
+#include <iomanip>
+#include <chrono>
+#include <ctime>
+#include <thread>
 //my includes
 #include "Polygon.h"
 
 
-
 int main() {
+    //std::clock_t c_start = std::clock();
+    //auto t_start = std::chrono::high_resolution_clock::now();
     int n = 0; // number of Vertexes of Polygon
     int n_dots = 0; // number of Dots
     std::cin >> n;
@@ -15,18 +19,29 @@ int main() {
     for (int i = 0; i < n; ++i) {
         double x, y;
         std::cin >> x >> y;
-        Vec v(x,y);
+        Vec v(x, y);
         vertexes.push_back(v);
     }
     Polygon pol(n, vertexes);
     std::cin >> n_dots;
     for (int j = 0; j < n_dots; ++j) {
-        double x,y;
+        double x, y;
         std::cin >> x >> y;
-        Vec v(x,y);
+        Vec v(x, y);
         dots.push_back(v);
     }
     std::vector<Vec> dots2;
+
     pol.Algorithm(pol, dots);
+
+    //std::clock_t c_end = std::clock();
+    //auto t_end = std::chrono::high_resolution_clock::now();
+    /*
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+              << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: "
+              << std::chrono::duration<double, std::milli>(t_end-t_start).count()
+              << " ms\n";
+    */
     return 0;
 }
